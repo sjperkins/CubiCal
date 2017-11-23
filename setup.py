0cf37ec3cb158c00ed4ae30a70a110812480d679
Simon Perkins <simon.perkins@gmail.com>
2017-11-23 18:20:58 +0200

    Break the numpy+cython setup.py dependency cycle
    Create a custom `BuildCommand(build_ext)` class that
    both creates and builds extensions in the `BuildCommand.run()` method,
    rather than creating them in the global scope of setup.py.
    By the time `BuildCommand.run()` is called, pip will have
    installed numpy and cython dependencies so the include and link flags
    can be correctly setup.

--- setup.py
+++ setup.py
@@ -33,101 +33,147 @@ from setuptools.command.build_ext import build_ext
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 log = logging.getLogger()
 
-# Try get location of numpy headers. Compilation requires these headers. 
+def _setup_extensions():
+  """
+  Deferred extension creation.
 
-try:
+  By this point, numpy and cython need to have been installed by pip/setuptools.
+  """
+  try:
     import numpy as np
-except ImportError:
-    include_path = ''
-else:
+  except ImportError:
+    log.exception("Install cannot proceed without numpy")
+    # Rethrow. We should *always* expect numpy install to succeed
+    raise
+  else:
     include_path = np.get_include()
 
-# Use Cython if available.
+  cmpl_args = ['-fopenmp',
+               '-ffast-math',
+               '-O2',
+               '-march=native',
+               '-mtune=native',
+               '-ftree-vectorize']
 
-try:
+  link_args = ['-lgomp']
+
+  try:
     from Cython.Build import cythonize
     import Cython.Compiler.Options as CCO
-except ImportError:
-    cythonize = None
-
-cmpl_args = ['-fopenmp',
-             '-ffast-math', 
-             '-O2', 
-             '-march=native',  
-             '-mtune=native', 
-             '-ftree-vectorize']
-    
-link_args = ['-lgomp']
-
-if cythonize:
-
+  except ImportError:
+    log.exception("Cython unavailable. Using bundled .c and .cpp files.")
+    have_cython = False
+  else:
     log.info("Cython is available. Cythonizing...")
+    have_cython = True
 
+  if have_cython:
     CCO.buffer_max_dims = 9
 
-    extensions = \
-        [Extension(
-            "cubical.kernels.cyfull_complex", ["cubical/kernels/cyfull_complex.pyx"],
+    extensions = (
+        [Extension("cubical.kernels.cyfull_complex", ["cubical/kernels/cyfull_complex.pyx"],
             include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
-         Extension(
-            "cubical.kernels.cyphase_only", ["cubical/kernels/cyphase_only.pyx"],
+         Extension("cubical.kernels.cyphase_only", ["cubical/kernels/cyphase_only.pyx"],
             include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
-         Extension(
-            "cubical.kernels.cyfull_W_complex", ["cubical/kernels/cyfull_W_complex.pyx"],
+         Extension("cubical.kernels.cyfull_W_complex", ["cubical/kernels/cyfull_W_complex.pyx"],
             include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
-         Extension(
-            "cubical.kernels.cychain", ["cubical/kernels/cychain.pyx"],
+         Extension("cubical.kernels.cychain", ["cubical/kernels/cychain.pyx"],
             include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
-         Extension(
-            "cubical.kernels.cytf_plane", ["cubical/kernels/cytf_plane.pyx"],
-            include_dirs=[include_path], language="c++", extra_compile_args=cmpl_args, 
+         Extension("cubical.kernels.cytf_plane", ["cubical/kernels/cytf_plane.pyx"],
+            include_dirs=[include_path], language="c++", extra_compile_args=cmpl_args,
             extra_link_args=link_args),
-         Extension(
-            "cubical.kernels.cyf_slope", ["cubical/kernels/cyf_slope.pyx"],
-            include_dirs=[include_path], language="c++", extra_compile_args=cmpl_args, 
+         Extension("cubical.kernels.cyf_slope", ["cubical/kernels/cyf_slope.pyx"],
+            include_dirs=[include_path], language="c++", extra_compile_args=cmpl_args,
             extra_link_args=link_args),
-         Extension(
-            "cubical.kernels.cyt_slope", ["cubical/kernels/cyt_slope.pyx"],
-            include_dirs=[include_path], language="c++", extra_compile_args=cmpl_args, 
-            extra_link_args=link_args)]
+         Extension("cubical.kernels.cyt_slope", ["cubical/kernels/cyt_slope.pyx"],
+            include_dirs=[include_path], language="c++", extra_compile_args=cmpl_args,
+            extra_link_args=link_args)])
 
-    extensions = cythonize(extensions, compiler_directives={'binding': True})
+    return cythonize(extensions, compiler_directives={'binding': True})
+  else:
+    return (
+        [Extension("cubical.kernels.cyfull_complex", ["cubical/kernels/cyfull_complex.c"],
+            include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
+         Extension("cubical.kernels.cyphase_only", ["cubical/kernels/cyphase_only.c"],
+            include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
+         Extension("cubical.kernels.cyfull_W_complex", ["cubical/kernels/cyfull_W_complex.c"],
+            include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
+         Extension("cubical.kernels.cychain", ["cubical/kernels/cychain.c"],
+            include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
+         Extension("cubical.kernels.cytf_plane", ["cubical/kernels/cytf_plane.cpp"],
+            include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
+         Extension("cubical.kernels.cyf_slope", ["cubical/kernels/cyf_slope.cpp"],
+            include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
+         Extension("cubical.kernels.cyt_slope", ["cubical/kernels/cyt_slope.cpp"],
+            include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args)])
 
-else:
+class BuildCommand(build_ext):
+  _custom_options = ('test',)
 
-    log.info("Cython unavailable. Using bundled .c and .cpp files.")
+  def initialize_options(self):
+    """ Initialise custom options """
 
-    extensions = \
-        [Extension("cubical.kernels.cyfull_complex", ["cubical/kernels/cyfull_complex.c"], 
-            include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
-         Extension("cubical.kernels.cyphase_only", ["cubical/kernels/cyphase_only.c"], 
-            include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
-         Extension("cubical.kernels.cyfull_W_complex", ["cubical/kernels/cyfull_W_complex.c"], 
-            include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
-         Extension("cubical.kernels.cychain", ["cubical/kernels/cychain.c"], 
-            include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
-         Extension("cubical.kernels.cytf_plane", ["cubical/kernels/cytf_plane.cpp"], 
-            include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
-         Extension("cubical.kernels.cyf_slope", ["cubical/kernels/cyf_slope.cpp"], 
-            include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args),
-         Extension("cubical.kernels.cyt_slope", ["cubical/kernels/cyt_slope.cpp"], 
-            include_dirs=[include_path], extra_compile_args=cmpl_args, extra_link_args=link_args)]
+    # Set any custom options to None
+    for o in self._custom_options:
+      setattr(self, o, None)
+
+    build_ext.initialize_options(self)
+
+  def run(self):
+    """
+    At this point pip will have installed our numpy and cython dependencies.
+    initialize_options and finalize_options will already have been run...
+
+    Now override run() to:
+    * Save any custom options created on the object
+    * Re-run self.initialize_options() to set custom options and self.extensions to None
+    * Create custom extensions through _setup_extensions()
+    * Re-run self.finalize_options() to run setuptools internal setup on the extensions.
+    * Re-apply any custom options to the object
+    """
+
+    # Save any of our custom options
+    saved_options = { o: getattr(self, o) for o in self._custom_options }
+
+    # Reset the build_ext options here
+    # In particular this sets self.extensions to None
+    self.initialize_options()
+
+    try:
+      # Override setup.py ext_modules with our actual extensions here
+      self.distribution.ext_modules = _setup_extensions()
+    except Exception:
+      log.exception("Exception creating extensions")
+      raise
+
+    # Now re-run finalize options to re-create the self.extensions
+    self.finalize_options()
+
+    # Re-apply any custom options
+    for o, v in saved_options.items():
+      setattr(self, o, v)
+
+    # Test that custom option is correctly set
+    assert self.test == 'test'
+
+    # Do the extension builds
+    build_ext.run(self)
 
 # Check for readthedocs environment variable.
-
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 
 if on_rtd:
-    requirements = ['numpy', 
-                    'cython', 
-                    'futures', 
+    requirements = ['numpy',
+                    'cython',
+                    'futures',
                     'matplotlib',
                     'scipy']
 else:
-    requirements = ['numpy', 
-                    'futures', 
-                    'python-casacore>=2.1.2', 
-                    'sharedarray', 
+    requirements = ['numpy',
+                    'cython',
+                    'futures',
+                    'python-casacore>=2.1.2',
+                    'sharedarray',
                     'matplotlib',
                     'scipy',
                     'astro-tigger']
@@ -135,7 +181,7 @@ else:
 setup(name='cubical',
       version='0.9.2',
       description='Fast calibration implementation exploiting complex optimisation.',
-      url='https://github.com/JSKenyon/phd-code',
+      url='https://github.com/ratt-ru/Cubical',
       classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
@@ -146,13 +192,18 @@ setup(name='cubical',
       author='Jonathan Kenyon',
       author_email='jonosken@gmail.com',
       license='GNU GPL v3',
-      cmdclass={'build_ext': build_ext},  
+      cmdclass={'build_ext': BuildCommand},
       packages=['cubical', 'cubical.machines', 'cubical.tools', 'cubical.kernels'],
       install_requires=requirements,
       include_package_data=True,
       zip_safe=False,
-      ext_modules = extensions,
-      entry_points={'console_scripts': ['gocubical = cubical.main:main']},           
+      # Make a dummy module to force call to build_ext
+      ext_modules = [Extension("DUMMY", [])],
+      # Pass a test option through to build_ext
+      options = {
+                  'build_ext' : {
+                    'test' : 'test'
+                  }
+                },
+      entry_points={'console_scripts': ['gocubical = cubical.main:main']},
 )
-
-
